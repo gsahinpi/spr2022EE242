1b66294db8e1d5b9f3cc16d7d20bea63d7362539
gsahinpi <gsahinpi@gmail.com>
2022-04-12 16:23:56 +0300

    what

---
+++ accel429inclass/Debug/Core/Src/i3g4250d.d
@@ -0,0 +1,4 @@
+Core/Src/i3g4250d.o: ../Core/Src/i3g4250d.c ../Core/Src/i3g4250d.h \
+ ../Core/Src/gyro.h
+../Core/Src/i3g4250d.h:
+../Core/Src/gyro.h:
---
+++ accel429inclass/Debug/Core/Src/l3gd20.d
@@ -0,0 +1,4 @@
+Core/Src/l3gd20.o: ../Core/Src/l3gd20.c ../Core/Src/l3gd20.h \
+ ../Core/Src/gyro.h
+../Core/Src/l3gd20.h:
+../Core/Src/gyro.h:
