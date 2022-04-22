df115434ab35c4c35b6e9d183ed54ec413ce9fae
gsahinpi <gsahinpi@gmail.com>
2022-04-22 13:07:46 +0300

    up

---
+++ hope2/Debug/Core/Src/lis302dl.d
@@ -0,0 +1,4 @@
+Core/Src/lis302dl.o: ../Core/Src/lis302dl.c ../Core/Inc/lis302dl.h \
+ ../Core/Inc/accelero.h
+../Core/Inc/lis302dl.h:
+../Core/Inc/accelero.h:
---
+++ hope2/Debug/Core/Src/lis3dsh.d
@@ -0,0 +1,4 @@
+Core/Src/lis3dsh.o: ../Core/Src/lis3dsh.c ../Core/Inc/lis3dsh.h \
+ ../Core/Inc/accelero.h
+../Core/Inc/lis3dsh.h:
+../Core/Inc/accelero.h:
